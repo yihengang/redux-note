399c5332cc8e27d6e43faf729f53c63eef43d3eb
unknown <casonyiheng@hotmail.com>
2023-09-04 22:31:24 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import noteReducer from "./reducers/noteReducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+// const renderApp = () => {
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// };
+
+// renderApp();
+// store.subscribe(renderApp);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
